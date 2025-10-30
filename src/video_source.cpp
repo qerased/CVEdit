b45987a7010e82087581e905613eb0e3a7160b8e
qerased <amaz11amaz@gmail.com>
2025-10-30 09:10:26 +0300

    NEW: cv: added video source class to stream video from webcam

---
+++ src/video_source.cpp
@@ -0,0 +1,50 @@
+#include "video_source.h"
+
+video_source::~video_source ()
+{
+    close ();
+}
+
+bool video_source::open_webcam (int index, std::optional<int> width, std::optional<int> height)
+{
+    close ();
+
+    if (!cap_.open (index, cv::CAP_ANY))
+        return false;
+
+    if (width) cap_.set (cv::CAP_PROP_FRAME_WIDTH, *width);
+    if (height) cap_.set (cv::CAP_PROP_FRAME_HEIGHT, *height);
+    return cap_.isOpened ();
+}
+
+bool video_source::is_opened () const
+{
+    return cap_.isOpened ();
+}
+
+void video_source::close ()
+{
+    if (cap_.isOpened ())
+        cap_.release ();
+}
+
+bool video_source::read (cv::Mat & out_img)
+{
+    if (!cap_.isOpened ()) return false;
+    return cap_.read (out_img);
+}
+
+double video_source::fps () const
+{
+    if (!cap_.isOpened ()) return 0.0;
+    return cap_.get (cv::CAP_PROP_FPS);
+}
+
+cv::Size video_source::frame_size () const
+{
+    if (!cap_.isOpened ()) return {};
+    int w = static_cast<int> (cap_.get (cv::CAP_PROP_FRAME_WIDTH));
+    int h = static_cast<int> (cap_.get (cv::CAP_PROP_FRAME_HEIGHT));
+    return cv::Size (w, h);
+}
+
---
+++ src/video_source.h
@@ -0,0 +1,30 @@
+#ifndef CVEDIT_VIDEO_SOURCE_H
+#define CVEDIT_VIDEO_SOURCE_H
+#include <optional>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+
+
+class video_source
+{
+public:
+    video_source () = default;
+    ~video_source ();
+
+    bool open_webcam (int index = 0,
+                      std::optional<int> width = std::nullopt,
+                      std::optional<int> height = std::nullopt);
+
+    bool is_opened () const;
+    void close ();
+
+    bool read (cv::Mat& out_img);
+
+    double fps () const;
+    cv::Size frame_size () const;
+private:
+    cv::VideoCapture cap_;
+};
+
+
+#endif //CVEDIT_VIDEO_SOURCE_H
