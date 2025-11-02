7b8e2a0e21f9ab7c49bb158cb7fa866ddd638397
qerased <amaz11amaz@gmail.com>
2025-11-02 11:49:55 +0300

    REF: qt: moved helper functions to separate file

---
+++ src/main_window_helpers.cpp
@@ -0,0 +1,62 @@
+#include "main_window.h"
+
+void main_window::bind_toggle (QCheckBox * chk, filter * f)
+{
+    connect(chk, &QCheckBox::toggled, this,
+            [this, f] (bool on) {
+                f->set_enabled (on);
+                if (!is_live_) reprocess_and_show ();
+            });
+}
+
+void main_window::bind_slider (QSlider * s, std::function<void(int)> setter)
+{
+    connect(s, &QSlider::valueChanged, this,
+            [this, setter] (int v) {
+                setter (v);
+                if (!is_live_) reprocess_and_show ();
+            });
+}
+
+void main_window::bind_spin (QSpinBox * s, std::function<void(int)> setter)
+{
+    connect(s, &QSpinBox::valueChanged, this,
+        [this, setter](int v)
+        {
+            setter (v);
+            if (!is_live_) reprocess_and_show ();
+        });
+}
+
+void main_window::bind_combo (QComboBox * c, std::function<void(const QVariant &)> setter)
+{
+    connect (c, qOverload<int> (&QComboBox::currentIndexChanged), this,
+        [this, c, setter] (int idx)
+        {
+            setter (c->itemData (idx));
+            if (!is_live_) reprocess_and_show ();
+        });
+}
+
+QHBoxLayout * main_window::get_chk_ord_layout (QGroupBox * box, filter * f, QCheckBox * chk, QSpinBox * spin)
+{
+    auto * gen_layout = new QHBoxLayout ();
+
+    chk = new QCheckBox ("Enable", box);
+    chk->setChecked (false);
+    bind_toggle (chk, f);
+
+    gen_layout->addWidget (chk);
+
+    auto * ord_layout = new QHBoxLayout ();
+    ord_layout->addWidget (new QLabel ("Order :", box));
+    spin = new QSpinBox (box);
+    spin->setRange (0, 300);
+    spin->setValue (0);
+    bind_spin (spin, [fil = f] (int o) { fil->set_num_order (o); });
+
+    ord_layout->addWidget (spin);
+
+    gen_layout->addLayout (ord_layout);
+    return gen_layout;
+}
